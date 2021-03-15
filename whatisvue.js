14053efd6386b8295b911d7983952fd00c1236a2
alexrem1 <remidy@live.co.uk>
2021-03-15 14:36:50 +0000

    What is vue?

---
+++ whatisvue.js
@@ -0,0 +1,12 @@
+// Vue is a javascript and typescript framework for creating fast, reliable and interactive web apps and single page applications (SPA) eg all routing is done in the browser and not on the server
+
+// SPAs
+// only a single HTML page sent (initially) to the broswer
+// vue intercepts subsequent requests and handles "page" changes in the browser by swapping what components are shown on the page
+// results in a much faster smoother website experience
+
+// Vue can be used for either standalone widgets, which we can drop into a website, or it can be used to create full websites
+
+// widgets eg signup form, search bar etc - self contained inside this vue component allowing it be reused in many different places
+
+//

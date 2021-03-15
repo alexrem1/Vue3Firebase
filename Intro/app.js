c8a334c4fccc77551f9079c7e66f9f3ff998f8b7
alexrem1 <remidy@live.co.uk>
2021-03-15 19:51:54 +0000

    Other Mouse Events

--- Intro/app.js
+++ Intro/app.js
@@ -7,6 +7,9 @@ const app = Vue.createApp({
       age: 45,
       addSomething: "",
       showBooks: true,
+      showSomething: true,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
@@ -15,9 +18,20 @@ const app = Vue.createApp({
     },
     addJohn(addName) {
       this.addSomething = addName;
+      this.showSomething = !this.showSomething;
     },
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });

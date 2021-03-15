e422001c4cafa6c46b86aa2accb82229fba4e614
alexrem1 <remidy@live.co.uk>
2021-03-15 17:39:21 +0000

    New features

---
+++ Intro/newfeatures.js
@@ -0,0 +1,11 @@
+// Composition API - Improves on reusability, organization and readability by giving us a access to a setup() function inside of components
+
+// We can now have MULTIPLE ROOT ELEMENTS for a component template eg elements side by side such as two divs. This reduces the amount of unnecesary HTML code
+
+// Teleport component - render content from one component in a different place in the DOM eg modals
+
+// Suspense component - used to handle asynchronous components easily and can provide fall-back content (eg a custom spinner) until data is loaded
+
+// Typescript support.
+
+// Multiple v-models for custom components, improved reactivity, performance gains

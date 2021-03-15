588bd1eb75a3e8490efd7fda3535de7244e86ce2
alexrem1 <remidy@live.co.uk>
2021-03-15 18:11:42 +0000

    Creating a vue app

--- Intro/app.js
+++ Intro/app.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const app = Vue.createApp({
+  //data functions
+  //   template: "<h2>I am the template</h2>" // instead create template directly
+});
+
+app.mount("#app");

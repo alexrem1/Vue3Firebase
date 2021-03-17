ac11ba0b52f5b43b1097f95a84f577dad7c6009d
alexrem1 <remidy@live.co.uk>
2021-03-17 02:02:43 +0000

    Component styles and global styles

--- VueCLI_Bigger_Projects/modal-project/src/components/Modal.vue
+++ VueCLI_Bigger_Projects/modal-project/src/components/Modal.vue
@@ -2,11 +2,12 @@
   <div class="backdrop">
     <div class="modal">
       <p>modal content</p>
+      <h1>modal content</h1>
     </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 .modal {
   width: 400px;
   padding: 20px;
@@ -20,5 +21,22 @@
   background: rgba(0, 0, 0, 0.5);
   width: 100%;
   height: 100%;
+}
+
+/* because this style is further down in the html documents than app.vue "style" */
+/* add "scoped" to "<style>" tag to only effect this component but not the root component*/
+/* OR */
+/* make selector more specific .modal h1 {} */
+/* global.css in assets for common elements that appear in multiple different components */
+
+h1 {
+  color: blue;
+  border: none;
+  padding: 0px;
+}
+
+/* overwrite global css */
+.modal p {
+  font-style: normal;
 }
 </style>
--- VueCLI_Bigger_Projects/modal-project/src/main.js
+++ VueCLI_Bigger_Projects/modal-project/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./assets/global.css";
 
 createApp(App).mount("#app");
 // the root component is "App" and we're mounting it to "#app", an element in the DOM

596ae46b7a9cbf3dfca37d69a61d8ffe365cc4a9
alexrem1 <remidy@live.co.uk>
2021-03-17 01:28:43 +0000

    Vue Files & Templates

--- VueCLI_Bigger_Projects/modal-project/src/App.vue
+++ VueCLI_Bigger_Projects/modal-project/src/App.vue
@@ -1,19 +1,22 @@
+// every vue component can contain 3 different parts but must contain a template
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      title: "My first vue app :)",
+    };
+  },
+};
 </script>
 
+// these styles are global and apply to any element on the page. They can be
+scoped to just this component eg
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,5 +25,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
--- VueCLI_Bigger_Projects/modal-project/src/main.js
+++ VueCLI_Bigger_Projects/modal-project/src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+// the root component is "App" and we're mounting it to "#app", an element in the DOM

6759cf5c0fa1a2c4f6f7808b7453613b4e25bac0
alexrem1 <remidy@live.co.uk>
2021-03-17 17:41:07 +0000

    Reaction timer - finishing touches

--- Projects/reaction-timer/src/components/Results.vue
+++ Projects/reaction-timer/src/components/Results.vue
@@ -1,5 +1,6 @@
 <template>
   <p>Reaction time: {{ score }}ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
@@ -8,7 +9,27 @@
 // - output the score inside the results component
 export default {
   props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Rapid reflexes";
+    } else if (this.score < 400) {
+      this.rank = "Twitter fingers";
+    } else {
+      this.rank = "Snails pace";
+    }
+  },
 };
 </script>
 
-<style></style>
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>

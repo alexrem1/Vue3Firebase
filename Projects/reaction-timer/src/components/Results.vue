38776813d58409920c09871bf57d3b88e62c2b5f
alexrem1 <remidy@live.co.uk>
2021-03-17 17:33:08 +0000

    Reaction timer - Challenge: showing a results component

---
+++ Projects/reaction-timer/src/components/Results.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Reaction time: {{ score }}ms</p>
+</template>
+
+<script>
+// Challenge
+// - when the game ends, show the results component
+// - output the score inside the results component
+export default {
+  props: ["score"],
+};
+</script>
+
+<style></style>

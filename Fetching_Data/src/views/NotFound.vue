e2f830d0c9d0f9019334267d84a8cc99be514dd8
alexrem1 <remidy@live.co.uk>
2021-03-19 17:51:55 +0000

    Using JSON server

---
+++ Fetching_Data/src/views/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>404</h2>
+  <h3>PAGE NOT FOUND</h3>
+</template>
---
+++ Fetching_Data/src/views/jobs/JobDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Job Details Page</h1>
+  <p>The Job ID is {{ id }}</p>
+  <!-- OR $route.params.id OR via data or via PROPS-->
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  //   data() {
+  //     return {
+  //       id: this.$route.params.id,
+  //     };
+  //   },
+};
+</script>
+
+<style></style>

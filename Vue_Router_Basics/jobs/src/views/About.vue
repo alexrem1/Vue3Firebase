02eba23d2c87649e6b466676f797828e1d25ebdd
alexrem1 <remidy@live.co.uk>
2021-03-19 16:35:02 +0000

    Router Links

--- Vue_Router_Basics/jobs/src/views/About.vue
+++ Vue_Router_Basics/jobs/src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos
+      maxime doloremque ullam? Provident ducimus blanditiis minima eaque.
+      Repudiandae vitae magnam corrupti iure, similique consequatur corporis
+      distinctio doloribus quaerat sequi culpa nemo, accusamus saepe animi
+      quibusdam voluptates! Libero excepturi non explicabo quasi dolorum cum
+      quod. Atque ex ad labore soluta doloremque, corporis laudantium officia
+      nemo tempora quae impedit dolorem dignissimos blanditiis voluptas maiores
+      distinctio facere. Nesciunt aperiam maxime neque deleniti possimus
+      repellendus quasi fuga veniam animi, fugiat esse officia? Sit, ratione.
+      Obcaecati accusamus rerum quos veniam corporis unde minus molestias
+      voluptatibus, sapiente inventore modi voluptates, soluta quisquam eos
+      possimus quae?
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos
+      maxime doloremque ullam? Provident ducimus blanditiis minima eaque.
+      Repudiandae vitae magnam corrupti iure, similique consequatur corporis
+      distinctio doloribus quaerat sequi culpa nemo, accusamus saepe animi
+      quibusdam voluptates! Libero excepturi non explicabo quasi dolorum cum
+      quod. Atque ex ad labore soluta doloremque, corporis laudantium officia
+      nemo tempora quae impedit dolorem dignissimos blanditiis voluptas maiores
+      distinctio facere. Nesciunt aperiam maxime neque deleniti possimus
+      repellendus quasi fuga veniam animi, fugiat esse officia? Sit, ratione.
+      Obcaecati accusamus rerum quos veniam corporis unde minus molestias
+      voluptatibus, sapiente inventore modi voluptates, soluta quisquam eos
+      possimus quae?
+    </p>
   </div>
 </template>
--- Vue_Router_Basics/jobs/src/views/Home.vue
+++ Vue_Router_Basics/jobs/src/views/Home.vue
@@ -1,20 +1,42 @@
 <template>
-  <p>DOG</p>
-
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Homepage</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos
+      maxime doloremque ullam? Provident ducimus blanditiis minima eaque.
+      Repudiandae vitae magnam corrupti iure, similique consequatur corporis
+      distinctio doloribus quaerat sequi culpa nemo, accusamus saepe animi
+      quibusdam voluptates! Libero excepturi non explicabo quasi dolorum cum
+      quod. Atque ex ad labore soluta doloremque, corporis laudantium officia
+      nemo tempora quae impedit dolorem dignissimos blanditiis voluptas maiores
+      distinctio facere. Nesciunt aperiam maxime neque deleniti possimus
+      repellendus quasi fuga veniam animi, fugiat esse officia? Sit, ratione.
+      Obcaecati accusamus rerum quos veniam corporis unde minus molestias
+      voluptatibus, sapiente inventore modi voluptates, soluta quisquam eos
+      possimus quae?
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos
+      maxime doloremque ullam? Provident ducimus blanditiis minima eaque.
+      Repudiandae vitae magnam corrupti iure, similique consequatur corporis
+      distinctio doloribus quaerat sequi culpa nemo, accusamus saepe animi
+      quibusdam voluptates! Libero excepturi non explicabo quasi dolorum cum
+      quod. Atque ex ad labore soluta doloremque, corporis laudantium officia
+      nemo tempora quae impedit dolorem dignissimos blanditiis voluptas maiores
+      distinctio facere. Nesciunt aperiam maxime neque deleniti possimus
+      repellendus quasi fuga veniam animi, fugiat esse officia? Sit, ratione.
+      Obcaecati accusamus rerum quos veniam corporis unde minus molestias
+      voluptatibus, sapiente inventore modi voluptates, soluta quisquam eos
+      possimus quae?
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>

4244295bb3aed3984105b1b89b1ea8f834a79cda
alexrem1 <remidy@live.co.uk>
2021-03-19 15:48:18 +0000

    Submitting the form

--- Forms_Data_Binding/web-form/src/components/SignupForm.vue
+++ Forms_Data_Binding/web-form/src/components/SignupForm.vue
@@ -1,11 +1,13 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
+    <!--prevent default-->
     <label>Email:</label>
     <input type="email" required v-model="email" />
     <!-- "v-model" for two way data binding -->
     <label>Password:</label>
     <input type="password" required v-model="password" />
     <input type="twoWay" required v-model="twoWay" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -24,6 +26,10 @@
     <div class="terms">
       <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
+    </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
     </div>
     <!-- OR -->
     <!-- <div>
@@ -58,6 +64,7 @@ export default {
       //   names: [],
       tempSkill: "",
       skills: [],
+      passwordError: "",
     };
   },
   methods: {
@@ -73,6 +80,21 @@ export default {
       this.skills = this.skills.filter((item) => {
         return skill !== item;
       });
+    },
+    handleSubmit() {
+      // validate password
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 chars long";
+      if (!this.passwordError) {
+        // make request to database to save user
+        console.log("email: ", this.email);
+        console.log("password: ", this.password);
+        console.log("role: ", this.role);
+        console.log("skills: ", this.skills);
+        console.log("terms accepted: ", this.terms);
+      }
     },
   },
 };
@@ -124,5 +146,22 @@ input[type="checkbox"] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>

104562f840649b9cc6e12fb47c96fd48fa0c298f
alexrem1 <remidy@live.co.uk>
2021-03-22 17:37:50 +0000

    Project planner - using material icons

--- Projects/project-planner/src/components/SingleProject.vue
+++ Projects/project-planner/src/components/SingleProject.vue
@@ -2,6 +2,11 @@
   <div class="project">
     <div class="actions">
       <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-icons">edit</span>
+        <span class="material-icons">delete</span>
+        <span class="material-icons">done</span>
+      </div>
     </div>
     <div v-if="showDetails" class="details">
       <p>{{ project.details }}</p>
@@ -37,5 +42,19 @@ export default {
 }
 h3 {
   cursor: pointer;
+}
+.actions {
+  display: flex; /* when something displays is flex box, it takes the immediate children(H3 and DIV) and it puts them next to each other on the same row. */
+  justify-content: space-between;
+  align-items: center;
+}
+.material-icons {
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+}
+.material-icons:hover {
+  color: #777;
 }
 </style>

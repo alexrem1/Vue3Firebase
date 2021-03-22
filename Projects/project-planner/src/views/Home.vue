11083eea0c4bf67033fdb8e888cba910f596a933
alexrem1 <remidy@live.co.uk>
2021-03-22 19:28:39 +0000

    Project planner - CHALLENGE showing filtered projects

--- Projects/project-planner/src/views/Home.vue
+++ Projects/project-planner/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <FilterNav @filterChange="current = $event" :current="current" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject
           :project="project"
           @delete="handleDelete"
@@ -14,6 +14,11 @@
 </template>
 
 <script>
+// challenge
+// - when the filter changes (current), only show those projects
+// - eg if we click "completed" only show completed projects
+// - use a computed property called filteredProjects to do this
+
 // @ is an alias to /src
 import SingleProject from "../components/SingleProject.vue";
 import FilterNav from "../components/FilterNav.vue";
@@ -44,6 +49,17 @@ export default {
         return project.id === id;
       });
       p.complete = !p.complete;
+    },
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === "completed") {
+        return this.projects.filter((project) => project.complete);
+      }
+      if (this.current === "ongoing") {
+        return this.projects.filter((project) => !project.complete);
+      }
+      return this.projects;
     },
   },
 };

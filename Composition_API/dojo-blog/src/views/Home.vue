c8c2472e8b580827525993fb561d28f5f4eeb600
alexrem1 <remidy@live.co.uk>
2021-03-23 15:37:08 +0000

    Refs vs Reactive

--- Composition_API/dojo-blog/src/views/Home.vue
+++ Composition_API/dojo-blog/src/views/Home.vue
@@ -1,55 +1,44 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p ref="para">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click me</button>
-    <button @click="age++">Increase age</button>
-    <input type="text" v-model="name" />
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }} - {{ nameOne }}</p>
+    <button @click="updateNinjaOne">Update ninja one</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
+    <button @click="updateNinjaTwo">Update ninja two</button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
-// @ is an alias to /src
-
+import { reactive, ref } from "vue";
 export default {
   name: "Home",
-  // setup function will run before any of the lifecycle hooks eg mounted/created
   setup() {
-    // "this" keyword is not available in the setup eg this.$refs.whateverTheRefIs
-    console.log(this); // undefined
+    const ninjaOne = ref({ name: "mario", age: 30 });
+    const ninjaTwo = reactive({ name: "luigi", age: 35 });
 
-    // template refs in composition API work different eg auto imports "ref" from "vue"
-    const para = ref(null);
-    console.log(para, para.value); // would still be null eg not "returned" yet meaning it's not associated with the dom element yet
-    // works with "handleClick" as once the button is clicked, we've already returned it at that point
-    console.log(para, para.value);
+    // reactive cannot be used on primitive values
+    const nameOne = ref("mario"); // will be reactive
+    const nameTwo = reactive("luigi"); // will NOT be reactive
 
-    // below is not reactive values eg values not automatically updating
-    // let name = "mario";
-    // let age = 30;
-
-    // unless using "refs"
-    const name = ref("mario");
-    const age = ref(30);
-
-    const handleClick = () => {
-      console.log("you clicked me");
-      console.log(para, para.value); // shows the ref object and value eg the dom element (p tag) and normal js properties/methods can be used on it
-      para.value.classList.add("test");
-      para.value.textContent = "hello guys";
-
-      // name = "luigi"; // does not change in the template as "name" and "age" above is not reactive
-      name.value = "luigi"; // now we are updating the values of these refs
-      age.value = "35"; // since refs are reactive, it'll update in the template
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40;
+      ninjaOne.value.name = "donkey";
+      nameOne.value = "Reactive";
     };
-
-    return { name, age, handleClick, para };
-  },
-  data() {
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 45;
+      ninjaTwo.name = "Loco";
+      // nameTwo = "manny" or nameTwo = reactive("manny") This will not work as its not reactive
+    };
     return {
-      // reactive value
-      score: 40,
+      ninjaOne,
+      ninjaTwo,
+      updateNinjaOne,
+      updateNinjaTwo,
+      nameOne,
+      nameTwo,
     };
   },
 };

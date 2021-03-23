0744a94d9181bc68b90cf81198498dd6e9dc61b4
alexrem1 <remidy@live.co.uk>
2021-03-23 15:48:52 +0000

    Computed Values in the Setup Function

--- Composition_API/dojo-blog/src/views/Home.vue
+++ Composition_API/dojo-blog/src/views/Home.vue
@@ -1,45 +1,28 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }} - {{ nameOne }}</p>
-    <button @click="updateNinjaOne">Update ninja one</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <input type="text" v-model="search" />
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      <p>{{ name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from "vue";
+import { ref, computed } from "vue";
+
 export default {
   name: "Home",
   setup() {
-    const ninjaOne = ref({ name: "mario", age: 30 });
-    const ninjaTwo = reactive({ name: "luigi", age: 35 });
+    const search = ref("");
+    const names = ref(["mario", "lunat", "betsy", "maria", "koola", "peach"]);
 
-    // reactive cannot be used on primitive values
-    const nameOne = ref("mario"); // will be reactive
-    const nameTwo = reactive("luigi"); // will NOT be reactive
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
 
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40;
-      ninjaOne.value.name = "donkey";
-      nameOne.value = "Reactive";
-    };
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45;
-      ninjaTwo.name = "Loco";
-      // nameTwo = "manny" or nameTwo = reactive("manny") This will not work as its not reactive
-    };
-    return {
-      ninjaOne,
-      ninjaTwo,
-      updateNinjaOne,
-      updateNinjaTwo,
-      nameOne,
-      nameTwo,
-    };
+    return { names, search, matchingNames };
   },
 };
 </script>

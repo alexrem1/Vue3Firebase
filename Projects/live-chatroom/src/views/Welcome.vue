b6732da6cbdfe983ebf5631e6c8f436acf8b12b7
alexrem1 <remidy@live.co.uk>
2021-03-25 00:03:23 +0000

    Live chatroom project - setup context & emitting events

--- Projects/live-chatroom/src/views/Welcome.vue
+++ Projects/live-chatroom/src/views/Welcome.vue
@@ -3,7 +3,7 @@
     <p>Welcome</p>
     <div v-if="showLogin">
       <h2>Login</h2>
-      <LoginForm />
+      <LoginForm @login="enterChat" />
       <p>
         No account yet?
         <span @click="showLogin = false">Sign up</span>
@@ -12,7 +12,7 @@
     </div>
     <div v-else>
       <h2>Sign up</h2>
-      <SignupForm />
+      <SignupForm @signup="enterChat" />
       <p>
         Already registered?
         <span @click="showLogin = true">Login</span>
@@ -25,13 +25,19 @@
 <script>
 import SignupForm from "../components/SignupForm.vue";
 import LoginForm from "../components/LoginForm.vue";
+import { useRouter } from "vue-router";
 import { ref } from "vue";
 export default {
   components: { SignupForm, LoginForm },
   setup() {
     const showLogin = ref(true);
+    const router = useRouter();
 
-    return { showLogin };
+    const enterChat = () => {
+      router.push({ name: "Chatroom" });
+    };
+
+    return { showLogin, enterChat };
   },
 };
 </script>

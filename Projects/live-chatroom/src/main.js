7d9e675629b702d37efea46f792e15a085ba87f0
alexrem1 <remidy@live.co.uk>
2021-03-27 05:36:29 +0000

    Live chatroom project - Waiting for firebase auth to initialize

--- Projects/live-chatroom/src/main.js
+++ Projects/live-chatroom/src/main.js
@@ -3,6 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
 
-createApp(App)
-  .use(router)
-  .mount("#app");
+// import firebase auth service
+import { projectAuth } from "./firebase/config";
+
+let app;
+
+projectAuth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App)
+      .use(router)
+      .mount("#app");
+  }
+});
+// the function above is going to fire when we first establish that connection and it's going to figure out the current user.
+// So we're waiting for that initial user value before we're rendering the app.
+// when we come to render the app and the function above fires at that point, "projectAuth.currentUser" will be accurate because we've already waited for that connection so it won't be null even though the user is logged in

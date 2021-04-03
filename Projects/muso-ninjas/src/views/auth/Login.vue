30d17d33117c462a481c458ecb7de567367d9260
alexrem1 <remidy@live.co.uk>
2021-04-03 09:17:28 +0100

    Project Muso Ninjas - creating a login view

---
+++ Projects/muso-ninjas/src/views/auth/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h3>Login</h3>
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <div class="error" v-if="error">{{ error }}</div>
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import useLogin from "@/composables/useLogin"; // instead of "../../composables/useLogin" because @ represents the root of the project relative to the source folder
+import { ref } from "vue";
+export default {
+  setup() {
+    const { error, login } = useLogin();
+
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = async () => {
+      const res = await login(email.value, password.value);
+      if (!error.value) {
+        console.log("User logged in");
+      }
+    };
+
+    return { email, password, handleSubmit, error };
+  },
+};
+</script>
+
+<style></style>

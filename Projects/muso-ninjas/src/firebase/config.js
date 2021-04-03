7124892973ce9bb097db44ea2076b14ac82637d4
alexrem1 <remidy@live.co.uk>
2021-04-03 07:51:08 +0100

    Project Muso Ninjas - connecting to firebase

---
+++ Projects/muso-ninjas/src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCGQOFOnlYOv1GsxHoIamgocEC_dKzAoU",
+  authDomain: "muso-ninjas-f4256.firebaseapp.com",
+  projectId: "muso-ninjas-f4256",
+  storageBucket: "muso-ninjas-f4256.appspot.com",
+  messagingSenderId: "259526558779",
+  appId: "1:259526558779:web:b1ceeac0632d30b4c9481e",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };

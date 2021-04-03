57ca52559f58c1c16be1edf3bba506ac5332af36
alexrem1 <remidy@live.co.uk>
2021-04-03 23:54:54 +0100

    Project Muso Ninjas - challenge: getting real-time playlist data

--- Projects/muso-ninjas/src/views/Home.vue
+++ Projects/muso-ninjas/src/views/Home.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="home">
-    <p>Home</p>
+    <div v-if="error" class="error">Could not fetch the data</div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id">{{ doc.title }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+// challenge
+//  - get realtime data from the playlist collection
+//  - output 2 conditional items - error or playlist tiles
+import getCollection from "../composables/getCollection";
 
 export default {
-  name: "Home",
-  components: {},
+  setup() {
+    const { error, documents } = getCollection("playlists");
+
+    return { error, documents };
+  },
 };
 </script>

dac1a2b04ba196d4f11be9e561e87a14dfc0948c
alexrem1 <remidy@live.co.uk>
2021-04-04 22:34:15 +0100

    Project Muso Ninjas - playlist details template

--- Projects/muso-ninjas/src/views/playlists/PlaylistDetails.vue
+++ Projects/muso-ninjas/src/views/playlists/PlaylistDetails.vue
@@ -1,10 +1,33 @@
 <template>
-  <h2>playlist details. ID - {{ id }}</h2>
+  <div v-if="error">{{ error }}</div>
+  <div v-if="playlist" class="playlist-details">
+    <!-- playlist information -->
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="playlist.coverUrl" />
+      </div>
+      <h2>{{ playlist.title }}</h2>
+      <p class="username">Created by: {{ playlist.userName }}</p>
+      <p class="description">{{ playlist.description }}</p>
+    </div>
+
+    <!-- song list -->
+    <div class="song-list">
+      <p>Song list here</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import getDocument from "@/composables/getDocument";
+
 export default {
   props: ["id"],
+  setup(props) {
+    const { error, document: playlist } = getDocument("playlists", props.id);
+
+    return { error, playlist };
+  },
 };
 </script>
 

ba9650f91a868da2b51f6458f4902700c3ff0049
alexrem1 <remidy@live.co.uk>
2021-04-04 21:35:34 +0100

    Project Muso Ninjas - listing playlists

--- Projects/muso-ninjas/src/views/Home.vue
+++ Projects/muso-ninjas/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div v-if="error" class="error">Could not fetch the data</div>
     <div v-if="documents">
-      <div v-for="doc in documents" :key="doc.id">{{ doc.title }}</div>
+      <ListView :playlists="documents" />
     </div>
   </div>
 </template>
@@ -12,8 +12,10 @@
 //  - get realtime data from the playlist collection
 //  - output 2 conditional items - error or playlist tiles
 import getCollection from "../composables/getCollection";
+import ListView from "../components/ListView.vue";
 
 export default {
+  components: { ListView },
   setup() {
     const { error, documents } = getCollection("playlists");
 

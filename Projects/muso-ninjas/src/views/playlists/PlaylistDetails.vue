4a5a265551ed08382f38c96610f93bc84215f90f
alexrem1 <remidy@live.co.uk>
2021-04-05 13:45:47 +0100

    Project Muso Ninjas - Adding songs to playlists

--- Projects/muso-ninjas/src/views/playlists/PlaylistDetails.vue
+++ Projects/muso-ninjas/src/views/playlists/PlaylistDetails.vue
@@ -15,11 +15,13 @@
     <!-- song list -->
     <div class="song-list">
       <p>Song list here</p>
+      <AddSong v-if="ownership" :playlist="playlist" />
     </div>
   </div>
 </template>
 
 <script>
+import AddSong from "@/components/AddSong.vue";
 import useStorage from "@/composables/useStorage";
 import useDocument from "@/composables/useDocument";
 import getDocument from "@/composables/getDocument";
@@ -29,6 +31,7 @@ import { useRouter } from "vue-router";
 
 export default {
   props: ["id"],
+  components: { AddSong },
   setup(props) {
     const { error, document: playlist } = getDocument("playlists", props.id);
     const { user } = getUser();
@@ -53,4 +56,43 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.playlist-details {
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 80px;
+}
+.cover {
+  overflow: hidden;
+  border-radius: 20px;
+  position: relative;
+  padding: 160px;
+}
+.cover img {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 90%;
+  min-height: 100%;
+  max-width: 200%;
+  max-height: 200%;
+}
+.playlist-info {
+  text-align: center;
+}
+.playlist-info h2 {
+  text-transform: capitalize;
+  font-size: 28px;
+  margin-top: 20px;
+}
+.playlist-info p {
+  margin-bottom: 20px;
+}
+.username {
+  color: #999;
+}
+.description {
+  text-align: left;
+}
+</style>

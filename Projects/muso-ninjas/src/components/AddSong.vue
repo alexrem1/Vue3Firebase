9c80014570298950448795a5bb912b17f49a2d89
alexrem1 <remidy@live.co.uk>
2021-04-05 15:33:16 +0100

    Project Muso Ninjas - firestore queries

--- Projects/muso-ninjas/src/components/AddSong.vue
+++ Projects/muso-ninjas/src/components/AddSong.vue
@@ -30,8 +30,8 @@ export default {
       await updateDoc({
         songs: [...props.playlist.songs, newSong],
       });
-      title.value("");
-      artist.value("");
+      title.value = "";
+      artist.value = "";
     };
 
     return { title, artist, showForm, handleSubmit };
